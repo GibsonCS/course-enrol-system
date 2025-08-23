9c5af68aca3e831e872aaeb8633088122dc44b09
GibsonCS <gibson.cruzds@gmail.com>
2025-08-23 00:23:05 -0300

    test: implements create-user test

---
+++ test/unit-test/create-user.test.ts
@@ -0,0 +1,17 @@
+import CreateUser from '../../src/application/use-cases/create-user.ts'
+import User from '../../src/domain/entities/user.ts'
+import UserRepositoryMemory from '../../src/infrastructure/database/user-repository-memory.ts'
+import {test, expect} from '@jest/globals'
+import { randomUUID } from 'node:crypto'
+
+const userRepositoryMemory = new UserRepositoryMemory()
+
+test('should create a user and return a uuid', async () => {
+    const createUser = new CreateUser(userRepositoryMemory)
+    
+    const result = await createUser.execute({name:'GIbson',cpf:'15675357718',email:'gibson@gmail.com',password:"5445"})
+
+    expect(result).toEqual(expect.objectContaining({
+        userId: expect.any(String)
+    }))
+})
