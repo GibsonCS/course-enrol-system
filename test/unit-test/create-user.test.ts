0ee590f2942a68ad93173958f09596d1d5e69a1c
GibsonCS <gibson.cruzds@gmail.com>
2025-08-26 20:55:11 -0300

    test: mock repositories from create-user-test

--- test/unit-test/create-user.test.ts
+++ test/unit-test/create-user.test.ts
@@ -1,18 +1,22 @@
 
+import { randomUUID } from 'node:crypto'
 import CreateUser from '../../src/application/use-cases/create-user.ts'
 import UserRepositoryPostgres from '../../src/infrastructure/repositories/user-repository-postgres.ts'
-import { test, expect} from '@jest/globals'
-
+import { test, expect, jest} from '@jest/globals'
+import User from 'src/domain/entities/user.ts'
 
 const userRepositoryPostgres = new UserRepositoryPostgres()
 
 const createUser = new CreateUser(userRepositoryPostgres)
 
 test('should create a user and return a uuid', async () => {
-    const result = await createUser.execute({name:'GIbson',cpf:'15675357718',email:'gibson8fla@gmail.com',password:"5445"})
+    jest.spyOn(userRepositoryPostgres,'findByEmail').mockResolvedValueOnce(undefined)
+    jest.spyOn(userRepositoryPostgres, 'save').mockResolvedValue(randomUUID())
 
-    expect(result).toEqual(expect.objectContaining({
-        userId: expect.any(Number)
+    const result = await createUser.execute({name:'Gibson',cpf:'15675357718', email:'email@email.com' ,password:"5445"})
+
+     expect(result).toEqual(expect.objectContaining({
+        userID: expect.any(String)
     }))
 })
 
@@ -22,5 +26,15 @@ test('should return a error to send a name less than 3', async () => {
 
 
 test('should return a error if a email has already been register', async () => {
-   expect(async () => await createUser.execute({name:'Gibs',cpf:'15675357718',email:'gibson@gmail.com',password:"5445"})).rejects.toThrow('Email has already been register') 
+    const user = {
+        id: randomUUID(),
+        name: 'Gibson',
+        cpf: '',
+        email: '',
+        password: ''
+    } as User
+
+    jest.spyOn(userRepositoryPostgres,'findByEmail').mockResolvedValue(user)
+
+    await expect(async () => await createUser.execute({name:'Gibs',cpf:'15675357718',email:'gibson@gmail.com',password:"5445"})).rejects.toThrow('Email has already been register') 
 })
