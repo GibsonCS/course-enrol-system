c22fb27ba674a434f0be33a3c012dcff02606e4a
GibsonCS <gibson.cruzds@gmail.com>
2025-08-26 20:57:35 -0300

    chore(core) change user id type to UUID

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,32 +1,39 @@
+import { UUID } from "node:crypto"
+
 export default class User {
+    private _id: UUID
     private _name: string
     private _cpf: string
     private _email: string
     private _password: string
-   
-    constructor(name: string, cpf: string, email: string, password: string){
-        if(name.length <= 3) {
-            throw new Error('The name must be three or more characters')
-        }
 
+    constructor({id, name, cpf, email, password}: {id: UUID, name: string, cpf: string, email: string, password:string}){
+        if(name.length <= 3) throw new Error('The name must be three or more characters')
+        
+        this._id = id
         this._name = name
         this._cpf = cpf
         this._email = email
         this._password = password
     }
 
+    get id() {
+        return this._id
+    }
+
     get name(){
         return this._name
     }
+
     get cpf() {
         return this._cpf
     }
+
     get email() {
         return this._email
     }
+
     get password() {
         return this._password
-    }
-
-  
+    }  
 }
