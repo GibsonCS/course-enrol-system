03543965074e75f122fa8e374a2dbb37b4c44ea2
GibsonCS <gibson.cruzds@gmail.com>
2025-08-26 21:03:08 -0300

    feat(infra): implements save and findAll method

--- src/infrastructure/repositories/user-repository-postgres.ts
+++ src/infrastructure/repositories/user-repository-postgres.ts
@@ -1,15 +1,26 @@
-import User from "src/domain/entities/user.ts";
-import { UserRepository } from "src/domain/repositories/user-repository.ts";
-import { db } from '../databases/postgres/db.ts';
+import { UUID } from "node:crypto";
+import User from "../../domain/entities/user.ts";
+import { type UserRepository } from "../../domain/repositories/user-repository.ts";
+import { db, } from '../databases/postgres/db.ts';
 import { usersTable } from '../databases/postgres/schemas/user-schema.ts';
+import { eq } from "drizzle-orm";
 
 export default class UserRepositoryPostgres implements UserRepository {
-    async save(item: User): Promise<Number> {
-        const {name, cpf,email,password} = item
+
+    async findByEmail(email: string): Promise<User | undefined> {
+        const [result] = await db.select().from(usersTable).where(eq(usersTable.email, email))
+
+        if(!result) return undefined
+
+        return new User({id: result.id as UUID, name: result.name, cpf: result.cpf, email: result.email, password: result.password})
+    }
+
+    async save(item: User): Promise<UUID> {
+        const {id, name, cpf, email, password } = item
         
-        const result = await db.insert(usersTable).values({name, cpf, email, password}).returning()
+        const result = await db.insert(usersTable).values({id, name, cpf, email, password}).returning()
         
-        return result[0].id
+        return result[0].id as UUID
     }
 
     async findAll(): Promise<Array<User>> {
