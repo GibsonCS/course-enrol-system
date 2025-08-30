e78d5838a5789bccc143ead09c254164c50b234c
GibsonCS <gibson.cruzds@gmail.com>
2025-08-29 21:17:40 -0300

    feat(core): add delete method in base repository

--- src/domain/repositories/base-respository.ts
+++ src/domain/repositories/base-respository.ts
@@ -1,8 +1,9 @@
-import { UUID } from "node:crypto"
+import { type UUID } from "node:crypto"
 
 export interface BaseRepository<T> {
     save(item: T): Promise<UUID>
     findAll(): Promise<Array<T>>
-    findByID(id: string | number): Promise<T | undefined>
+    findByID(id: UUID): Promise<T | undefined>
     update(item: T): Promise<void>
+    delete(id: UUID): Promise<void>
 }
