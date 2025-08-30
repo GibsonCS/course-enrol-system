d00bbacba1baeb730c44a6a172396a680a7b2c9e
GibsonCS <gibson.cruzds@gmail.com>
2025-08-29 21:15:46 -0300

    feat(infra): implements delete function in user-repository-postgres

--- src/infrastructure/repositories/user-repository-postgres.ts
+++ src/infrastructure/repositories/user-repository-postgres.ts
@@ -1,4 +1,4 @@
-import { UUID } from "node:crypto";
+import {type UUID } from "node:crypto";
 import User from "../../domain/entities/user.ts";
 import { type UserRepository } from "../../domain/repositories/user-repository.ts";
 import { db, } from '../databases/postgres/db.ts';
@@ -6,6 +6,12 @@ import { usersTable } from '../databases/postgres/schemas/user-schema.ts';
 import { eq } from "drizzle-orm";
 
 export default class UserRepositoryPostgres implements UserRepository {
+    async delete(id: UUID): Promise<void> {
+        await db.delete(usersTable).where(eq(usersTable.id, id)).returning()
+
+
+    }
+
     async updateName(id: UUID, name: string): Promise<void> {
         await db.update(usersTable).set({name}).where(eq(usersTable.id, id))
     }
@@ -14,7 +20,7 @@ export default class UserRepositoryPostgres implements UserRepository {
        throw new Error('method not implemented')
     }
 
-    async findByID(id: string): Promise<User | undefined> {
+    async findByID(id: UUID): Promise<User | undefined> {
         const result = await db.select().from(usersTable).where(eq(usersTable.id, id))
         
         if(!result[0]) return undefined
